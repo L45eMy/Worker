1d7712b5336d09c38d632d1f6accfb496282b9b5
L45eMy <ruben.larisch@fau.de>
2016-09-01 06:46:24 +0200

    Reschedule new App versions for Dioscope Analysis

--- scheduler.py
+++ scheduler.py
@@ -3,8 +3,10 @@
 import logging
 import json
 import requests
+import dateutil.parser
 
 from backend import Backend
+from elasticsearch import Elasticsearch
 
 logging.basicConfig(level=logging.WARNING)
 
@@ -43,18 +45,30 @@ class Scheduler(object):
 			},
 		}
 
+	@classmethod
+	def _default_dioscopejob(cls):
+		return {
+			'type':'dioscope',
+			'state':'pending',
+			'jobInfo': {
+				'appType':'AppStoreApp'
+			}
+		}
 
 	def __init__(self, backendUrl):
 		self.backend = Backend(backendUrl)
 
-	def schedule_job(self, jobDict):
-		job = Scheduler._default_runjob()
+	def schedule_job(self, jobDict, type='run'):
+		if type == 'dioscope':
+			job = Scheduler._default_dioscopejob()
+		else:
+			job = Scheduler._default_runjob()
 		job = dict_merge(job, jobDict)
 		jobId = self.backend.post_job(job)
 		return jobId
 
 
-	def schedule_bundleId(self, bundleId, worker=None, device=None, account=None, country=None, executionStrategy=None):
+	def schedule_bundleId(self, bundleId, worker=None, device=None, account=None, country=None, executionStrategy=None, type='run'):
 		jobDict = {
 			'jobInfo': {
 				'bundleId':bundleId
@@ -70,7 +84,7 @@ class Scheduler(object):
 			jobDict['jobInfo']['storeCountry'] = country
 		if executionStrategy:
 			jobDict['jobInfo']['executionStrategy'] = executionStrategy
-		return self.schedule_job(jobDict)
+		return self.schedule_job(jobDict, type)
 
 
 	def schedule_appId(self, appId, account=None, country=None, executionStrategy=None):
@@ -103,6 +117,40 @@ class Scheduler(object):
 			if not self.schedule_bundleId(entry['id']['attributes']['im:bundleId'], account=account, country=country, executionStrategy=None):
 				result = False
 		return result
+	
+	def schedule_dioscope_new(self, host, index, account=None, country=None, executionStrategy=None):
+		es = Elasticsearch([host])
+		result = es.search(index=index, doc_type="app", fields="bundleId.untouched,currentVersionReleaseDate,version", size=1000)
+		apps = result['hits']['hits']
+		count = 0
+		result = True
+		for app in apps:
+			fields = app['fields']
+			bundleId = fields['bundleId.untouched'][0]
+			oldVersion = fields['version'][0]
+			oldVersionReleaseDate = dateutil.parser.parse(fields['currentVersionReleaseDate'][0])
+			url = 'http://itunes.apple.com/lookup?bundleId=%s&country=%s' % (bundleId, country)
+			r = requests.get(url)
+			if r.status_code != 200:
+				logging.error("Requests to %s failed: %s", url, r.text)
+				return False
+			resDict = json.loads(r.text)
+			if 'results' not in resDict or len(resDict['results']) == 0:
+				logging.error("%s not found in App Store", bundleId)
+				continue
+			
+			newVersion = resDict['results'][0]['version']
+			newVersionReleaseDate = dateutil.parser.parse(resDict['results'][0]['currentVersionReleaseDate'])
+
+			if newVersionReleaseDate > oldVersionReleaseDate:
+				count += 1
+
+				logger.debug("%s (%s -> %s)", bundleId, oldVersion, newVersion)
+				if not self.schedule_bundleId(bundleId, account=account, country=country, executionStrategy=executionStrategy, type='dioscope'):
+					result = False
+		
+		logger.info("%d jobs scheduled", count)
+		return result
 
 
 
@@ -122,8 +170,11 @@ def main():
 	mutalCmds.add_argument('--appId', type=int, metavar='trackId', help='the apps appstore id')
 	mutalCmds.add_argument('--itunes-top', type=int, default=10, nargs='?', metavar='n', help='use the top N free apps (defaults to 10)')
 	mutalCmds.add_argument('--itunes-new', type=int, default=10, nargs='?', metavar='n', help='use the top N new (and free) apps (defaults to 10)')
+	mutalCmds.add_argument('--dioscope-new', action='store_true', help='schedule jobs for new versions from apps in Elasticsearch index (max 1000 apps)')
 	cmdGroup.add_argument('--itunes-genre', type=int, metavar='id', help='use the given genre only (defaults to all)')
 	cmdGroup.add_argument('--itunes-country', type=str, default="de", nargs='?', metavar='countryCode', help='the store country to use (defaults to "de")')
+	cmdGroup.add_argument('--dioscope-host', type=str, default="localhost", help='the Elasticsearch host for --dioscope-new')
+	cmdGroup.add_argument('--dioscope-index', type=str, default="dioscope", help='the Elasticsearch index for --dioscope-new')
 
 
 	args = parser.parse_args()
@@ -147,18 +198,23 @@ def main():
 		printRes(res)
 		return
 
+	if 'dioscope_new' in args and args.dioscope_new:
+		res = scheduler.schedule_dioscope_new(args.dioscope_host, args.dioscope_index, account=args.account, country=args.itunes_country, executionStrategy=args.strategy)
+		printRes(res)
+    	return
+
 	genre = ''
 	if args.itunes_genre:
 		genre = 'genre=%i' % args.itunes_genre
 
 	if 'itunes_top' in args and args.itunes_top:
-		url = 'https://itunes.apple.com/%s/rss/topfreeapplications/limit=%i/genre=%s/json' % (args.itunes_country, args.itunes_top, genre)
+		url = 'https://itunes.apple.com/%s/rss/topfreeapplications/limit=%i/%s/json' % (args.itunes_country, args.itunes_top, genre)
 		res = scheduler.schedule_itunes(url, account=args.account, country=args.itunes_country, executionStrategy=args.strategy)
 		printRes(res)
 		return
 
 	if 'itunes_new' in args and args.itunes_new:
-		url = 'https://itunes.apple.com/%s/rss/newfreeapplications/limit=%i/genre=%s/json' % (args.itunes_country, args.itunes_new, genre)
+		url = 'https://itunes.apple.com/%s/rss/newfreeapplications/limit=%i/%s/json' % (args.itunes_country, args.itunes_new, genre)
 		res = scheduler.schedule_itunes(url, account=args.account, country=args.itunes_country, executionStrategy=args.strategy)
 		printRes(res)
 		return
